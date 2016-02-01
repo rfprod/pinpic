bc6ca04d90cdc87eba598569e23af80213b14092
V <zoidenmacht@zoho.com>
2016-02-01 20:53:43 +0000

    post new poll ready

--- app/models/polls.js
+++ app/models/polls.js
@@ -4,14 +4,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Poll = new Schema({
-	poll: {
-		id: String,
-		owner: String,
-		displayName: String,
-		question: String,
-        options: Array,
-        votes: Array
-	}
+	_id: String,
+	owner: String,
+	displayName: String,
+	question: String,
+    options: Array,
+    votes: Array,
+    timestamp: String
 });
 
 module.exports = mongoose.model('Poll', Poll);
--- app/routes/index.js
+++ app/routes/index.js
@@ -29,8 +29,49 @@ module.exports = function (app, passport) {
 		res.sendFile(path + '/public/profile.html');
 	});
 	app.route(/pollpost/).post(isLoggedIn, function(req, res){
-    	console.log('res: '+JSON.stringify(req.body));
-    	res.send("you posted: "+JSON.stringify(req.body));
+    	//console.log('req: '+JSON.stringify(req.body));
+    	var pollOwner = req.body.owner;
+    	var pollName = req.body.name;
+    	var pollQuestion = req.body.question;
+    	var pollOptions = req.body.options.replace(/ , /g,',').replace(/ ,/g,',').replace(/, /g,',').split(',');
+    	var pollVotes = [];
+    	for (var i=0;i<pollOptions.length;i++) pollVotes.push(0);
+    	// get current date
+		var dateLog = "";
+			var date = new Date();
+			var year = date.getFullYear();
+			var month = date.getMonth()+1;
+			if (month <10) month = "0"+month;
+			var day = date.getDate();
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			if (minutes <10) minutes = "0"+minutes;
+			dateLog = year+"-"+month+"-"+day+" "+hours+":"+minutes;
+    	var id = null;
+    	console.log('POST params: '+pollOwner+" | "+pollName+" | "+pollQuestion+" | "+JSON.stringify(pollOptions)+" | "+JSON.stringify(pollVotes)+" | "+dateLog);
+    	Polls.find({}, function(err,data){
+	    	if (err) throw err;
+	        console.log('"polls" collection: '+JSON.stringify(data));
+	        id = data.length + 1;
+	        console.log('next id: '+id);
+		    // prepare new record
+		    var newPoll = new Polls();
+			newPoll._id = id;
+			newPoll.owner = pollOwner;
+			newPoll.displayName = pollName;
+			newPoll.question = pollQuestion;
+			newPoll.options = pollOptions;
+			newPoll.votes = pollVotes;
+			newPoll.timestamp = dateLog;
+			newPoll.save(function (err) {
+				if (err) throw err;
+				console.log('data saved');
+			});
+			console.log(newPoll);
+	    });
+    	//res.send("you posted: "+JSON.stringify(req.body));
+    	req.session.valid = true;
+  		res.redirect('/profile');
 	}); 
 	app.route('/api/:id').get(isLoggedIn, function (req, res) {res.json(req.user.github);});
 	app.route('/auth/github').get(passport.authenticate('github'));
